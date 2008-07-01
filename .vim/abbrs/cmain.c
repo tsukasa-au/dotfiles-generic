2c99acae9ea63ac4a812c18512a8754ff95724f6
James Bunton <jamesbunton@fastmail.fm>
2008-07-01 21:21:56 +1000

    Vim: Updates to abbrs
     * C - added extra includes by default, adjusted formatting.
     * Added C++ main() skeleton.
     * Python - made more useful by default.

--- .vim/abbrs/cmain.c
+++ .vim/abbrs/cmain.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <inttypes.h>
 #include <stdbool.h>
 
 
-int main(void) {
+int
+main(void)
+{
 	___
 	return 0;
 }
--- .vim/abbrs/cmaina.c
+++ .vim/abbrs/cmaina.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <inttypes.h>
 #include <stdbool.h>
 
 
-int main(int argc, char** argv) {
+int
+main(int argc, char** argv)
+{
 	___
 	return 0;
 }
